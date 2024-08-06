8fb1ae099a97acb9aa7699a54eb8f81f0d1b9d04
Mercado-cristian <cm6239759@gmail.com>
2024-08-06 20:09:24 -0300

    tarea completa

--- calculadoraimpedancia.vb
+++ calculadoraimpedancia.vb
@@ -1,5 +1,19 @@
-﻿Imports Microsoft.VisualBasic
+﻿Module Module1
 
-Public Class Class1
+    Sub Main()
+        Dim B1 As Single
+        Dim B2 As Single
+        Dim h As Single
+        Dim A As Single
+        Console.Write("intruduzca la base menor:")
+        B1 = Console.ReadLine()
+        Console.Write("intruduzca la base mayor:")
+        B2 = Console.ReadLine()
+        Console.Write("intruduzca la altura:")
+        h = Console.ReadLine()
+        A = (B1 + B2) * (h / 2)
+        Console.WriteLine("el area del trapecio es:" & A)
+        Console.ReadLine()
+    End Sub
 
-End Class
+End Module
